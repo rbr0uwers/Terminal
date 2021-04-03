2a0dac5c8e4e5ae536b5244351dc91f83b0aa138
Ronny Brouwers <ronny.brouwers@icloud.com>
2021-04-03 14:51:57 +0200

    Project renamed

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('consolero app is running!');
+    expect(await page.getTitleText()).toEqual('terminal app is running!');
   });
 
   afterEach(async () => {
