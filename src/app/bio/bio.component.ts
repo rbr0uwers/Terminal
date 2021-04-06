0ece3e8558575598a3b741cf433fe22230957e10
Ronny Brouwers <ronny.brouwers@icloud.com>
2021-04-06 11:55:31 +0200

    Bio added

--- src/app/bio/bio.component.ts
+++ src/app/bio/bio.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren} from '@angular/core';
 
 @Component({
   selector: 'app-bio',
   templateUrl: './bio.component.html',
-  styleUrls: ['./bio.component.css']
+  styleUrls: ['./bio.component.css'],
+  host: {
+    '(window:resize)': 'onResize($event)'
+  }
 })
-export class BioComponent implements OnInit {
+export class BioComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  onResize(event: Event) {
+    this.progressBars.forEach(bar => bar.nativeElement.innerHTML = this.createProgressBar(bar));
+  }
+
+  ngAfterViewInit(): void {
+    this.progressBars.forEach(bar => bar.nativeElement.innerHTML = this.createProgressBar(bar));
+  }
+
+  @ViewChildren("progressBar") progressBars!: QueryList<ElementRef>;
+
+  createProgressBar(bar: ElementRef) : string {
+    let charsToFill = Math.floor(bar.nativeElement.clientWidth / 8) - 2; //Minus "[]"
+    let barCharsLength = Math.floor(charsToFill * parseFloat(bar.nativeElement.dataset["pbValue"]));
+    let whiteSpaceCharLength = charsToFill - barCharsLength;
+
+    return `[${"=".repeat(barCharsLength)}${"&nbsp;".repeat(whiteSpaceCharLength)}]`;;
   }
 
 }
