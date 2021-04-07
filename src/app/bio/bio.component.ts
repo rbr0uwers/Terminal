b8b7704f354f0888adfb0d74391741762d4fb44c
Ronny Brouwers <ronny.brouwers@icloud.com>
2021-04-07 09:49:48 +0200

    Comment added

--- src/app/bio/bio.component.ts
+++ src/app/bio/bio.component.ts
@@ -4,9 +4,7 @@ import { AfterViewInit, Component, ElementRef, QueryList, ViewChildren} from '@a
   selector: 'app-bio',
   templateUrl: './bio.component.html',
   styleUrls: ['./bio.component.css'],
-  host: {
-    '(window:resize)': 'onResize($event)'
-  }
+  host: {'(window:resize)': 'onResize($event)'}
 })
 export class BioComponent implements AfterViewInit {
 
@@ -23,7 +21,9 @@ export class BioComponent implements AfterViewInit {
   @ViewChildren("progressBar") progressBars!: QueryList<ElementRef>;
 
   createProgressBar(bar: ElementRef) : string {
-    let charsToFill = Math.floor(bar.nativeElement.clientWidth / 8) - 2; //Minus "[]"
+    // 1 Monospace Char has approx width of 8px at 1rem height - Needs to be recalculated when changing font or font-size
+    // Minus "[]"
+    let charsToFill = Math.floor(bar.nativeElement.clientWidth / 8) - 2; 
     let barCharsLength = Math.floor(charsToFill * parseFloat(bar.nativeElement.dataset["pbValue"]));
     let whiteSpaceCharLength = charsToFill - barCharsLength;
 
